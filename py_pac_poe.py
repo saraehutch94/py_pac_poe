8b01aa146a890e573b64a3e0e9351c57722b9637
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-02-19 14:12:47 -0500

    add first function -> startGame

--- py_pac_poe.py
+++ py_pac_poe.py
@@ -1,8 +1,28 @@
 # Py Pac Poe
 
+# variables
+
+from re import X
+
+
 player = ""
+
 boardDictionary = {
     "a1": "", "a2": "", "a3": "",
     "b1": "", "b2": "", "b3": "",
     "c1": "", "c2": "", "c3": ""
 }
+
+# functions
+
+def startGame():
+
+    global player
+
+    print('''
+    ----------------------
+    Let's play Py Pac Poe!
+    ----------------------
+    ''')
+    playerChoice = input("Choose your player (X or O): ")
+    player = playerChoice
